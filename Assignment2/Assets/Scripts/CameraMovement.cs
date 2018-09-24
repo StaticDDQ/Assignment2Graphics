93a663f481912bd1776e88c65e975acee1c5581a
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-09-24 12:59:56 +1000

    Added camera and player movement in first person

---
+++ Assignment2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    private const float RANGE = 90;
+    [SerializeField] private float mouseSpeed;
+
+    private float pitch;
+    private float yaw;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Moving the pitch and yaw using mouse movement and applying a certain speed
+        yaw += mouseSpeed * Input.GetAxis("Mouse X");
+        pitch -= mouseSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(Mathf.Clamp(pitch,-RANGE, RANGE), yaw, 0.0f);
+    }
+}
---
+++ Assignment2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float Speed = 5f;
+    public float Gravity = -9.81f;
+    public float GroundDistance = 0.2f;
+    public LayerMask Ground;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool _isGrounded = true;
+    private Transform Cam;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cam = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+
+        Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        moveDir = Cam.TransformDirection(moveDir);
+        controller.Move(moveDir * Time.deltaTime * Speed);
+
+        velocity.y += Gravity * Time.deltaTime;
+
+        _isGrounded = Physics.CheckSphere(Cam.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        if (_isGrounded && velocity.y < 0)
+            velocity.y = 0f;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
