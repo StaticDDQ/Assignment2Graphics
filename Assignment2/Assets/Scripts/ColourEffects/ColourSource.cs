585db24b6f520b19d82f9d09d6b19e6b8f7be7a4
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-10-08 18:32:13 +1100

    Added colour source script, may not be needed depending on how player grabs the color

---
+++ Assignment2/Assets/Scripts/ColourEffects/ColourSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourSource : MonoBehaviour {
+
+    [SerializeField] private Color currColor;
+
+    // Use this for initialization
+    void Awake()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color",currColor);
+    }
+
+    public Color GetColor()
+    {
+        return this.currColor;
+    }
+}
