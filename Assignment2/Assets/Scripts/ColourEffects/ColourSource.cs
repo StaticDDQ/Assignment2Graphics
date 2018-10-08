fbcce24a5f346482a5c806c8a76ffff8140a7839
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-10-09 01:42:25 +1100

    updated energy capsule model

--- Assignment2/Assets/Scripts/ColourEffects/ColourSource.cs
+++ Assignment2/Assets/Scripts/ColourEffects/ColourSource.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ColourSource : MonoBehaviour {
 
+    [SerializeField] private GameObject target;
     [SerializeField] private Color currColor;
 
     // Use this for initialization
     void Awake()
     {
-        GetComponent<Renderer>().material.SetColor("_Color",currColor);
+        target.GetComponent<Renderer>().material.SetColor("_Color",currColor);
     }
 
     public Color GetColor()
