7c8b269cbfec6e95f73e8f4f371e90485098b6d4
Samuel Tumewa <stumewa@yahoo.com>
2018-10-10 06:50:31 +1100

    added ball shooting mechanic, to change to liquid sphere

--- Assignment2/Assets/Scripts/Player/ColorPickerRayCasting.cs
+++ Assignment2/Assets/Scripts/Player/ColorPickerRayCasting.cs
@@ -11,24 +11,26 @@ public class ColorPickerRayCasting : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-		if(Input.GetMouseButton(1) && Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceToSee) && hit.collider.tag == "Source")
+        if (Input.GetMouseButton(1) && Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceToSee) && hit.collider.tag == "Source")
         {
+            Debug.Log("Ray casted");
             var material = hit.collider.GetComponent<Renderer>().material;
             Color c = material.color;
+            Debug.Log(c.ToString());
 
             var tex2D = material.mainTexture as Texture2D;
             if (tex2D != null)
             {
                 c = tex2D.GetPixelBilinear(hit.textureCoord[0], hit.textureCoord[1]);
+                Debug.Log(c.ToString());
             }
             hasColor = true;
             colorPreview = c;
         }
-        else if(hasColor && Input.GetMouseButtonDown(0) && Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceToSee) && 
+        /*else if(hasColor && Input.GetMouseButtonDown(0) && Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceToSee) && 
             (hit.collider.tag == "PickUp" || hit.collider.tag == "Colourable"))
         {
             hit.collider.GetComponent<ColourDecider>().SetEffect(colorPreview);
-        }
+        }*/
 	}
 }
---
+++ Assignment2/Assets/Scripts/Player/PaintShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintShooting : MonoBehaviour {
+
+    public GameObject paintballPrefab;
+    public float paintballSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Transform paintballSpawn = Camera.main.transform;
+        Debug.Log(paintballSpawn.forward);
+        //paintballSpawn.localPosition += new Vector3(0.0f, -0.5f, 0.5f);
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            var paintball = (GameObject)Instantiate(paintballPrefab,
+                paintballSpawn.position + paintballSpawn.forward,
+                paintballSpawn.rotation);
+            paintball.GetComponent<Rigidbody>().velocity = paintballSpawn.forward * paintballSpeed;
+            paintball.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+
+            Destroy(paintball, 2.0f);
+        }
+	}
+}
