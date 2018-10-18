54be33b3ce38a0aaf9e8ecf321f72d78a726b1fc
cellayo <myoesman@student.unimelb.edu.au>
2018-10-18 22:24:35 +1100

    paintball create paintball scatter particle system on collision w colourable object, and ps creates paint splatter on ripples

--- Assignment2/Assets/Scripts/Paintball/PaintballCollision.cs
+++ Assignment2/Assets/Scripts/Paintball/PaintballCollision.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaintballCollision : MonoBehaviour {
+public class PaintballCollision : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject paintScatterPrefab;
+    private ParticleSystem currentPS;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     // When paintball has made contact with another collider
-    void OnCollisionEnter (Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision detected");
         GameObject collided = collision.gameObject;
@@ -23,6 +29,20 @@ public class PaintballCollision : MonoBehaviour {
         if (collided.tag == "PickUp" || collided.tag == "Colourable")
         {
             Debug.Log("Object colourable");
+
+            // Match the paintball color to the splatter
+            paintScatterPrefab.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", Camera.main.GetComponent<ColorPickerRayCasting>().colorPreview);
+
+            // Generates a paint splatter particle system on the collision point
+            ContactPoint collisionContact = collision.contacts[0];
+            var paintsplatter = (GameObject)Instantiate(paintScatterPrefab,
+                collisionContact.point,
+                Quaternion.identity);
+
+            ParticleSystem paintSplatterPS = paintsplatter.GetComponent<ParticleSystem>();
+            paintSplatterPS.Emit(10);
+            Destroy(paintSplatterPS, 5f);
+
             Color c = GetComponent<Renderer>().material.color;
             collided.GetComponent<ColourDecider>().SetEffect(c);
             Destroy(this.gameObject);
---
+++ Assignment2/Assets/Scripts/Paintball/PaintballSplatter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintballSplatter : MonoBehaviour
+{
+
+    private ParticleSystem currentPS;
+    public GameObject onePaintSplatterPrefab;
+    private ParticleCollisionEvent[] paintSplatterCollisions;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPS = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int collCount = currentPS.GetSafeCollisionEventSize();
+
+        paintSplatterCollisions = new ParticleCollisionEvent[collCount];
+
+        int eventCount = currentPS.GetCollisionEvents(other, paintSplatterCollisions);
+
+        onePaintSplatterPrefab.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", Camera.main.GetComponent<ColorPickerRayCasting>().colorPreview);
+
+        for (int i = 0; i < eventCount; i++)
+        {
+            Vector3 paintSplatterPos = paintSplatterCollisions[i].intersection;
+            var rotation = Quaternion.LookRotation(paintSplatterCollisions[i].normal);
+            Collider collider = (Collider)paintSplatterCollisions[i].colliderComponent;
+            if (other.tag == "PickUp" || other.tag == "Colourable") { }
+            else
+            {
+                Instantiate(onePaintSplatterPrefab, paintSplatterPos, rotation);
+            }
+        }
+
+    }
+}
