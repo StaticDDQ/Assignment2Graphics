53e1dd72a35c903651004182bec1744aed7cdaf9
Nicola Halim <staticddq@gmail.com>
2018-10-19 00:24:40 +1100

    added player, colour object, and canvas as a prefab. Made a spawner which spawns objects

---
+++ Assignment2/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnObject : MonoBehaviour {
+
+    [SerializeField] private GameObject colourObject;
+    [SerializeField] private int counter = 3;
+    [SerializeField] private Text displayText;
+
+    private void Start()
+    {
+        displayText.text = counter.ToString();
+    }
+
+    // Update is called once per frame
+    public void Spawn()
+    {
+        if (counter != 0)
+        {
+            Instantiate(colourObject, transform.position, Quaternion.identity);
+            displayText.text = (--counter).ToString();
+        }
+    }
+}
