37c64edda3442f9bb4b6b1cb23ac012707d1a4fe
Nicola Halim <staticddq@gmail.com>
2018-10-20 01:22:39 +1100

    added force in the slippery effect

--- Assignment2/Assets/Scripts/ColourEffects/Effects/SlipperyEffect.cs
+++ Assignment2/Assets/Scripts/ColourEffects/Effects/SlipperyEffect.cs
@@ -2,6 +2,8 @@
 
 public class SlipperyEffect : ColourEffect {
 
+    private float force = 5f;
+
     public override void ApplyEffect()
     {
         // Simply apply a slippery physics material to an object
@@ -14,6 +16,14 @@ public class SlipperyEffect : ColourEffect {
         };
 
         GetComponent<Collider>().material = slippery;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * force, ForceMode.Impulse);
+        }
     }
 
     public override void RevertEffect()
